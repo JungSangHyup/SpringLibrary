61abb9703d77eaa6d45f194b78bc40803307c949
JungSangHyup <51068026+JungSangHyup@users.noreply.github.com>
2021-09-16 16:14:31 +0900

    BookVO 추가

--- src/main/java/com/sample/library/domain/BookVO.java
+++ src/main/java/com/sample/library/domain/BookVO.java
@@ -3,5 +3,12 @@ package com.sample.library.domain;
 public class BookVO {
 	private int book_id;
 	private String book_name;
-	private int 
+	private int book_price;
+	private String book_des;
+	private String book_img;
+	private String book_pages;
+	private String book_isbn;
+	private String book_writer;
+	private int sales;
+	private String category_code;
 }
